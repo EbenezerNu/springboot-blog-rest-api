f06c8865ae2ee7abd79792e745e47025254fc2d2
Catalyst546 <37144172+Catalyst546@users.noreply.github.com>
2022-08-29 19:17:00 +0000

    Added method for role repository

--- src/main/java/com/springboot/blog/repository/RoleRepository.java
+++ src/main/java/com/springboot/blog/repository/RoleRepository.java
@@ -1,4 +1,11 @@
 package com.springboot.blog.repository;
 
-public class RoleRepository {
+import com.springboot.blog.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
 }
