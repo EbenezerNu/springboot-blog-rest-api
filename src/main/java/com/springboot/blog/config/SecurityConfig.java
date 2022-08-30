6c8cb30fabb5ecde6de052872a0a3379eea1ce60
Catalyst546 <37144172+Catalyst546@users.noreply.github.com>
2022-08-30 08:59:12 +0000

    Edited the security config class to implement custom user details from database

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.springboot.blog.config;
 
+import com.springboot.blog.security.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,6 +21,9 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
 
     @Bean
     PasswordEncoder passwordEncoder(){
@@ -37,6 +43,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     }
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    /*
+    @Override
     @Bean
     protected UserDetailsService userDetailsService() {
         UserDetails user = User.builder().username("catalyst")
@@ -46,5 +59,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .password(passwordEncoder().encode("aSdFgHjKl:"))
                 .roles("ADMIN").build();
         return new InMemoryUserDetailsManager(user, admin);
-    }
+    }*/
 }
--- src/main/java/com/springboot/blog/repository/UserRepository.java
+++ src/main/java/com/springboot/blog/repository/UserRepository.java
@@ -2,15 +2,21 @@ package com.springboot.blog.repository;
 
 import com.springboot.blog.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+
+    @Query(value = "SELECT * FROM USERS WHERE USERNAME =:usernameOrEmail OR EMAIL =:usernameOrEmail LIMIT 1", nativeQuery = true)
     Optional<User> findByUsernameOrEmail(String usernameOrEmail);
+
     Optional<User> findByUsername(String username);
 
     Boolean existsByEmail(String email);
-    Boolean existsByEmaiUsername(String username);
+    Boolean existsByUsername(String username);
 }
