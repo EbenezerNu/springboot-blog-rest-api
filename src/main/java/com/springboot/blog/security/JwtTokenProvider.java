2f8dac2e6c249f801249ec7a65bc480da3015e18
Catalyst546 <37144172+Catalyst546@users.noreply.github.com>
2022-09-07 08:58:08 +0000

    Created getUsername from JWT method

--- src/main/java/com/springboot/blog/security/JwtTokenProvider.java
+++ src/main/java/com/springboot/blog/security/JwtTokenProvider.java
@@ -1,5 +1,6 @@
 package com.springboot.blog.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,25 +13,36 @@ import java.util.Date;
 public class JwtTokenProvider {
 
     @Value("${app.jwt-secret}")
-    private String jwt_secret_key;
+    private String jwtSecretKey;
     @Value("${app.jwt-expiration-milliseconds}")
-    private int jwt_expiration_date;
+    private int jwtExpirationDate;
 
 
     //Generate token
     public String generateToken(Authentication authentication){
         String username = authentication.getName();
         Date currentDate = new Date();
-        Date expiryDate = new Date(currentDate.getTime() + jwt_expiration_date);
+        Date expiryDate = new Date(currentDate.getTime() + jwtExpirationDate);
 
         String token = Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(currentDate)
                 .setExpiration(expiryDate)
-                .signWith(SignatureAlgorithm.HS512, jwt_secret_key)
+                .signWith(SignatureAlgorithm.HS512, jwtSecretKey)
                 .compact();
         return token;
     }
+
+    // Get username from token;
+    public String getUsernameFromJWT(String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecretKey)
+                .parseClaimsJwt(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    //Validate Token
 
 
 }
