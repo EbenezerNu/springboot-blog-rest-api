2a2ea8225a63cac21b7677982125fe062f8a2b2e
Catalyst546 <37144172+Catalyst546@users.noreply.github.com>
2022-09-18 00:01:36 +0000

    Refined the User repository class

--- src/main/java/com/springboot/blog/repository/UserRepository.java
+++ src/main/java/com/springboot/blog/repository/UserRepository.java
@@ -10,12 +10,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email);
-
     @Query(value = "SELECT * FROM USERS WHERE USERNAME =:usernameOrEmail OR EMAIL =:usernameOrEmail LIMIT 1", nativeQuery = true)
     Optional<User> findByUsernameOrEmail(String usernameOrEmail);
-
-    Optional<User> findByUsername(String username);
 
     Boolean existsByEmail(String email);
 
