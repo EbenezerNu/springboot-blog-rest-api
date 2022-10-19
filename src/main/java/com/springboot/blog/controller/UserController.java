faec16ffe95df383778a6d3f455de39355ebdc54
Catalyst546 <37144172+Catalyst546@users.noreply.github.com>
2022-10-19 07:44:41 +0000

    Fixing change password, content-type not supported

--- src/main/java/com/springboot/blog/controller/UserController.java
+++ src/main/java/com/springboot/blog/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.springboot.blog.controller;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.springboot.blog.payload.SignUpDto;
 import com.springboot.blog.payload.UserDto;
 import com.springboot.blog.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.util.MultiValueMap;
@@ -17,6 +19,7 @@ import javax.validation.Valid;
 @RequestMapping("/api/v2/auth")
 @Slf4j
 @Valid
+
 public class UserController {
 
     private final UserService userService;
@@ -36,7 +39,7 @@ public class UserController {
         return new ResponseEntity<>(userService.createUser(signUpDto), HttpStatus.CREATED);
     }
 
-    @PatchMapping("/change-password")
+    @PostMapping(path="/change-password", consumes={"application/json"})
     public ResponseEntity<String> changePassword(@RequestBody MultiValueMap<String,String> params){
         log.info("inside change Password --> \n {}", params);
 //        signUpDto.setPassword(passwordEncoder.encode(signUpDto.getPassword()));
